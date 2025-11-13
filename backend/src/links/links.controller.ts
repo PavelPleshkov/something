23217055d806eeb80bf5f4bb126b81330b5b942d
Pavel Pleshkov <pleshkov.p@gmail.com>
2025-11-13 13:36:41 +0300

    feat: add links service to links controller

--- backend/src/links/links.controller.ts
+++ backend/src/links/links.controller.ts
@@ -3,53 +3,67 @@ import {
   Controller,
   Get,
   // HttpCode,
-  Param,
+  // Param,
   Post,
-  Query,
+  // Query,
   // Req,
 } from '@nestjs/common';
-import type { Request } from 'express';
-import { CreateLinkDto } from './createLink.dto';
+// import type { Request } from 'express';
+import { CreateLinkDto } from '../dto/createLink.dto';
+import { LinksService } from './links.service';
+import { Link } from 'src/interfaces/link.interface';
 
 @Controller('links')
 export class LinksController {
-  //http://localhost:3000/links
-  // @Get()
-  // // @Redirect('https://nestjs.com', 301)
-  // // findAll(): string {
-  // //   // findAll() {
-  // //   return `This action return all links`;
-  // //   // return { link: 'link1' };
-  // // }
-  // // findAll(@Req() request: Request): any {
-  // //   // console.log('Get request');
-  // //   return request;
-  // // }
-  // findAll(@Req() request: Request): string {
-  //   return `This action returns all links`;
-  // }
-
-  @Get()
-  async findAll(
-    @Query('isInner') isInner: boolean,
-    @Query('color') color: string,
-  ) {
-    return `This action returns all cats filtered by color: ${color} and isIner: ${isInner}`;
-  }
-
-  @Get(':id')
-  //http://localhost:3000/links/4
-  findOne(@Param() params: any): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} link`;
-  }
-  // findOne(@Param('id') id: string): string {
-  //   return `This action returns a #${id} link`;
-  // }
+  constructor(private linksService: LinksService) {}
 
   @Post()
-  // @HttpCode(204)
   async create(@Body() createLinkDto: CreateLinkDto) {
-    return `This action adds a new link`;
+    this.linksService.create(createLinkDto);
   }
+
+  @Get()
+  async findAll(): Promise<Link[]> {
+    return this.linksService.findAll();
+  }
+
+  // //http://localhost:3000/links
+  // // @Get()
+  // // // @Redirect('https://nestjs.com', 301)
+  // // // findAll(): string {
+  // // //   // findAll() {
+  // // //   return `This action return all links`;
+  // // //   // return { link: 'link1' };
+  // // // }
+  // // // findAll(@Req() request: Request): any {
+  // // //   // console.log('Get request');
+  // // //   return request;
+  // // // }
+  // // findAll(@Req() request: Request): string {
+  // //   return `This action returns all links`;
+  // // }
+
+  // @Get()
+  // async findAll(
+  //   @Query('isInner') isInner: boolean,
+  //   @Query('color') color: string,
+  // ) {
+  //   return `This action returns all cats filtered by color: ${color} and isIner: ${isInner}`;
+  // }
+
+  // @Get(':id')
+  // //http://localhost:3000/links/4
+  // findOne(@Param() params: any): string {
+  //   console.log(params.id);
+  //   return `This action returns a #${params.id} link`;
+  // }
+  // // findOne(@Param('id') id: string): string {
+  // //   return `This action returns a #${id} link`;
+  // // }
+
+  // @Post()
+  // // @HttpCode(204)
+  // async create(@Body() createLinkDto: CreateLinkDto) {
+  //   return `This action adds a new link`;
+  // }
 }
